f50deda29ce67ecd7ae7f34fb4b82aa53b644fe3
Jede <brahmajede@gmail.com>
2021-03-13 15:30:02 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {  
+    return string.toLowerCase();
+    console.log('HELLO');
+}
+function logShout(hello) {
+    console.log(hello.toUpperCase());
+    
+}
+function logWhisper(LADIDA) {
+    console.log(LADIDA.toLowerCase());
+    
+}
+function sayHiToGrandma (string) {
+    var hello = "I can't hear you!";
+    var HELLO = "YES INDEED!";
+    var LoveToo = "I love you, too.";
+    if (string.toLowerCase(string) === string) {
+      return hello;
+    }
+    else if (string.toUpperCase(string) === string) {
+      return HELLO;
+    }
+    else if ("I love you, Grandma." === string) {
+      return LoveToo
+    }
+  }
